cf567d553bb9ed1163616a8c533ae81c6c05566f
JadeTyrer <jadetyrer@hotmail.co.uk>
2020-02-23 15:12:42 +1100

    added views for index, new and show

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+5.times do
+  Event.create(
+    name: Faker::Music.album,
+    location: Faker::Address.city,
+    contact_email: Faker::Internet.email,
+    external_link: Faker::Internet.domain_name,
+    description: Faker::Lorem.paragraph
+)
+end 
+
